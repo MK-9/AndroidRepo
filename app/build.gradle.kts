abe1710475186f50ec45eb7ab9e2f60bf7aeb378
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-26 09:48:28 +0330

    increase kotlin extension version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,12 +45,13 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
+
     packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+//        resources {
+//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+//        }
     }
 }
 
