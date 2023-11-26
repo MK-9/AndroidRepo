362d3780379bb3bc4533c8f6a6275ddba329cb6e
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-26 09:41:43 +0330

    increase target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "ir.divar.androidtask"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ir.divar.androidtask"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
