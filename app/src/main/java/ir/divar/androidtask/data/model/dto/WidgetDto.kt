1194f828435501d0935ab5bdae6b36a09892acb6
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-27 09:29:07 +0330

    add Post List Models

---
+++ app/src/main/java/ir/divar/androidtask/data/model/dto/WidgetDto.kt
@@ -0,0 +1,8 @@
+package ir.divar.androidtask.data.model.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class WidgetDto(
+    @SerializedName("widget_type") val widgetType: String?,
+    @SerializedName("data") val data: DataDto?
+)
