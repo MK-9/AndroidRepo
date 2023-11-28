2efa9bf449e57a1a7186a9cd27f16679b96cff69
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-28 19:19:46 +0330

    fix DataDto by adding some fields

--- app/src/main/java/ir/divar/androidtask/data/model/dto/DataDto.kt
+++ app/src/main/java/ir/divar/androidtask/data/model/dto/DataDto.kt
@@ -4,10 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class DataDto(
     @SerializedName("title") val title: String?,
+    @SerializedName("subtitle") val subtitle: String?,
+    @SerializedName("text") val text: String?,
+    @SerializedName("value") val value: String?,
     @SerializedName("token") val token: String?,
     @SerializedName("price") val price: String?,
-    @SerializedName("subtitle") val subtitle: String?,
-    @SerializedName("thumbnail") val thumbnail: String?,
     @SerializedName("city") val city: String?,
-    @SerializedName("district") val district: String?
+    @SerializedName("district") val district: String?,
+    @SerializedName("image_url") val imageUrl: String?,
+    @SerializedName("show_thumbnail") val showThumbnail: Boolean?,
+    @SerializedName("thumbnail") val thumbnail: String?,
+    @SerializedName("items") val items: List<ItemDto>?,
 )
---
+++ app/src/main/java/ir/divar/androidtask/data/model/dto/ItemDto.kt
@@ -0,0 +1,3 @@
+package ir.divar.androidtask.data.model.dto
+
+data class ItemDto(val imageUrl: String)
