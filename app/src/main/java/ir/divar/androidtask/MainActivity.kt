32220683480ba1de458730564c373237c3970101
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-28 17:18:33 +0330

    test api is successful

--- app/src/main/java/ir/divar/androidtask/MainActivity.kt
+++ app/src/main/java/ir/divar/androidtask/MainActivity.kt
@@ -1,6 +1,7 @@
 package ir.divar.androidtask
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +11,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import ir.divar.androidtask.data.model.Result
+import ir.divar.androidtask.data.repository.PlaceRepository
+import ir.divar.androidtask.data.repository.PostRepository
 import ir.divar.androidtask.ui.theme.AndroidTaskTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var postRepository: PostRepository
+
+    @Inject
+    lateinit var placeRepository: PlaceRepository
+
+
+    companion object {
+        const val ACCESS_TOKEN =
+            "Basic YXBpa2V5OjY5Y1dxVW8wNGhpNFdMdUdBT2IzMmRXZXQjsllsVzBtSkNiwU9yLUxEamNDUXFMSzJnR29mS3plZg=="
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +47,31 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenCreated {
+            CoroutineScope(Dispatchers.Main).launch {
+                val result = placeRepository.getPlaceList(ACCESS_TOKEN)
+                when (result) {
+                    is Result.InProgress -> {
+                        Log.d("Result", "Progress")
+                    }
+
+                    is Result.OnSuccess -> {
+                        Log.d("Result", "OnSuccess")
+                        val dtp = result.data
+                        Log.d("Result", "city: size " + "${dtp.cities?.size}")
+
+                        dtp.cities?.forEach {
+                            Log.d("Result", "city: name " + "${it.name}")
+                        }
+                    }
+
+                    is Result.OnError -> {
+                        Log.d("Result", "OnError")
+                    }
                 }
             }
         }
