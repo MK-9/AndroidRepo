8778de07ff83bc74a85b643fc3d4027c1ff453b5
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-29 11:30:52 +0330

    create CityViewModel

---
+++ app/src/main/java/ir/divar/androidtask/feature/city/CityViewModel.kt
@@ -0,0 +1,75 @@
+package ir.divar.androidtask.feature.city
+
+import android.util.Log
+import androidx.compose.runtime.Immutable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.divar.androidtask.data.model.Result
+import ir.divar.androidtask.data.repository.PlaceRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@Immutable
+data class CityScreenUiState(
+    val isLoading: Boolean = false,
+    val data: List<CityItem> = emptyList(),
+    val message: String? = null
+)
+
+@Immutable
+data class CityItem(
+    val title: String?, val onCityClicked: (() -> Unit)?
+)
+
+@HiltViewModel
+class CityViewModel @Inject constructor(
+    val placeRepository: PlaceRepository
+) : ViewModel() {
+
+    private var _cityScreenUiState = MutableStateFlow(CityScreenUiState())
+    val cityScreenUiState = _cityScreenUiState.asStateFlow()
+
+    init {
+        launchCity()
+    }
+
+    private fun launchCity() {
+        viewModelScope.launch {
+            when (val result = placeRepository.getPlaceList(ACCESS_TOKEN)) {
+                is Result.InProgress -> {
+                    _cityScreenUiState.update { currentState ->
+                        currentState.copy(isLoading = true)
+                    }
+                }
+
+                is Result.OnSuccess -> {
+                    val items = result.data.cities?.map {
+                        CityItem(it.name, null)
+                    }
+
+                    items?.run {
+                        _cityScreenUiState.update { currentState ->
+                            currentState.copy(isLoading = false, data = this)
+                        }
+                    }
+                }
+
+                is Result.OnError -> {
+                    Log.d("Result", "OnError")
+                    _cityScreenUiState.update { currentState ->
+                        currentState.copy(isLoading = true)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val ACCESS_TOKEN =
+            "Basic YXBpa2V5OjY5Y1dxVW8wNGhpNFdMdUdBT2IzMmRXZXQjsllsVzBtSkNiwU9yLUxEamNDUXFMSzJnR29mS3plZg=="
+    }
+}
