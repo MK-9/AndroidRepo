1108e94e14bb7d9f01d1f2f16d85324fb632783c
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-29 08:04:25 +0330

    create screen sealed class

---
+++ app/src/main/java/ir/divar/androidtask/feature/screen/Screen.kt
@@ -0,0 +1,26 @@
+package ir.divar.androidtask.feature.screen
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+import ir.divar.androidtask.R
+
+sealed class Screen(
+    val route: String, @StringRes val resourceId: Int, val icon: ImageVector
+) {
+
+    data object City : Screen(
+        "store", R.string.city, Icons.Default.Home
+    )
+
+    data object Post : Screen(
+        "post", R.string.post, Icons.Default.ShoppingCart
+    )
+
+    data object PostDetails : Screen(
+        "postDetail", R.string.postDetails, Icons.Default.Place
+    )
+}
