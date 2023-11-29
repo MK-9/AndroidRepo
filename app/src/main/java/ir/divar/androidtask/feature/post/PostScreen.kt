035be2ed938367cdedfb5d7e33934d18f3f9659c
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-29 11:32:26 +0330

    init PostScreen

--- app/src/main/java/ir/divar/androidtask/feature/post/PostScreen.kt
+++ app/src/main/java/ir/divar/androidtask/feature/post/PostScreen.kt
@@ -1,9 +1,13 @@
 package ir.divar.androidtask.feature.post
 
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun PostScreen(navController: NavHostController) {
-
+    Surface {
+        Text("PostScreen")
+    }
 }
