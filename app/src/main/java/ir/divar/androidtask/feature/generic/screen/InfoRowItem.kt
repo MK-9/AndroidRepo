64e2c7ce3e00eec34ee5d7d2bf4ec2d2a2e7d814
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-30 10:09:41 +0330

    create InfoRowItem

--- app/src/main/java/ir/divar/androidtask/feature/generic/screen/InfoRowItem.kt
+++ app/src/main/java/ir/divar/androidtask/feature/generic/screen/InfoRowItem.kt
@@ -1,9 +1,62 @@
 package ir.divar.androidtask.feature.generic.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import ir.divar.androidtask.feature.generic.uiState.PostItem
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import ir.divar.androidtask.feature.generic.uiState.PostDataItem
 
 @Composable
-fun InfoRowItem(widget: PostItem) {
+fun InfoRowItem(widget: PostDataItem?) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+
+            //title
+            widget?.title?.run {
+                Text(
+                    text = this,
+                    modifier = Modifier
+                        .weight(0.66f, true)
+                        .padding(start = 16.dp, end = 16.dp, top = 8.dp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
+
+            //value
+            widget?.value?.run {
+                Text(
+                    text = this,
+                    modifier = Modifier
+                        .weight(0.33f, true)
+                        .padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 4.dp),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    textAlign = TextAlign.End,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
 }
