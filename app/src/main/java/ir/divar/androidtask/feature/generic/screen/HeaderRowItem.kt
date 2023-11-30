538841f81fc90fb16a2b1ae03b6d2fa89ef24fbd
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-11-30 09:33:56 +0330

    initalize screen for diiferent widget type

---
+++ app/src/main/java/ir/divar/androidtask/feature/generic/screen/HeaderRowItem.kt
@@ -0,0 +1,9 @@
+package ir.divar.androidtask.feature.generic.screen
+
+import androidx.compose.runtime.Composable
+import ir.divar.androidtask.feature.generic.uiState.PostItem
+
+@Composable
+fun HeaderRowItem(widget: PostItem) {
+
+}
--- app/src/main/java/ir/divar/androidtask/feature/generic/screen/PostScreenContent.kt
+++ app/src/main/java/ir/divar/androidtask/feature/generic/screen/PostScreenContent.kt
@@ -4,6 +4,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import ir.divar.androidtask.feature.generic.uiState.PostItem
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isDescriptionRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isHeaderRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isImageSliderRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isInfoRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isPostRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isSubtitleRow
+import ir.divar.androidtask.feature.generic.uiState.PostItemExtension.isTitleRow
 import ir.divar.androidtask.feature.generic.uiState.PostsData
 
 @Composable
@@ -13,16 +20,36 @@ fun PostScreenContent(data: PostsData?, onItemClicked: (PostItem) -> Unit) {
     LazyColumn {
         items(items = widgets) { widgetItem ->
             when {
-                widgetItem.widgetType == PostItem.WidgetType.TITLE_ROW.name -> {
+                widgetItem.isImageSliderRow() -> {
+                    ImageRowItem(widgetItem)
+                }
+
+                widgetItem.isHeaderRow() -> {
+                    HeaderRowItem(widgetItem)
+                }
+
+                widgetItem.isTitleRow() -> {
                     TitleRowItem(widgetItem)
                 }
 
-                widgetItem.widgetType == PostItem.WidgetType.SUBTITLE_ROW.name -> {
+                widgetItem.isSubtitleRow() -> {
                     SubtitleRowItem(widgetItem)
                 }
 
-                widgetItem.widgetType == PostItem.WidgetType.POST_ROW.name -> {
+                widgetItem.isDescriptionRow() -> {
+                    DescriptionRowItem(widgetItem)
+                }
+
+                widgetItem.isInfoRow() -> {
+                    InfoRowItem(widgetItem)
+                }
+
+                widgetItem.isPostRow() -> {
                     PostRowItem(widgetItem, onItemClicked)
+                }
+
+                widgetItem.isImageSliderRow() -> {
+                    ImageRowItem(widgetItem)
                 }
             }
         }
