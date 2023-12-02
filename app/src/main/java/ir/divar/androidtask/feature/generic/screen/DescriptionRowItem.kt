eb7f687581d271b87219afacd5047eabfb0ab088
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-02 07:40:58 +0330

    create description row item

--- app/src/main/java/ir/divar/androidtask/feature/generic/screen/DescriptionRowItem.kt
+++ app/src/main/java/ir/divar/androidtask/feature/generic/screen/DescriptionRowItem.kt
@@ -1,9 +1,35 @@
 package ir.divar.androidtask.feature.generic.screen
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ir.divar.androidtask.feature.generic.uiState.PostItem
 
 @Composable
 fun DescriptionRowItem(widget: PostItem) {
-
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = widget.data?.text ?: "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    }
 }
