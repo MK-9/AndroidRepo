db213ab53df7a77d7110d5571371075aa13cf06d
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-02 09:11:40 +0330

    create Description Row Item

--- app/src/main/java/ir/divar/androidtask/feature/generic/screen/DescriptionRowItem.kt
+++ app/src/main/java/ir/divar/androidtask/feature/generic/screen/DescriptionRowItem.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import ir.divar.androidtask.feature.generic.uiState.PostItem
 
@@ -28,7 +30,8 @@ fun DescriptionRowItem(widget: PostItem) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
-                style = MaterialTheme.typography.titleMedium,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Gray
             )
         }
     }
