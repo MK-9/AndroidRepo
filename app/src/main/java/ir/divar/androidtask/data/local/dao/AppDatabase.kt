edaadbc1e0e39ee796bfd86165fe4a5b9fe74875
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-04 10:34:38 +0330

    fix AppDatabase entity

--- app/src/main/java/ir/divar/androidtask/data/local/dao/AppDatabase.kt
+++ app/src/main/java/ir/divar/androidtask/data/local/dao/AppDatabase.kt
@@ -2,10 +2,9 @@ package ir.divar.androidtask.data.local.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import ir.divar.androidtask.data.local.entity.ImageItemEntity
 import ir.divar.androidtask.data.local.entity.PostEntity
 
-@Database(entities = [PostEntity::class, ImageItemEntity::class], version = 1)
+@Database(entities = [PostEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
 }
