d3fb239f6950ef9daa277fa7234218c6a209a766
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-04 14:31:01 +0330

    update city dao

--- app/src/main/java/ir/divar/androidtask/data/local/dao/CityDao.kt
+++ app/src/main/java/ir/divar/androidtask/data/local/dao/CityDao.kt
@@ -3,13 +3,24 @@ package ir.divar.androidtask.data.local.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import ir.divar.androidtask.data.local.entity.CityEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CityDao {
     @Insert
-    fun insertCity(cityEntity: CityEntity)
+    suspend fun insertCities(cityEntity: List<CityEntity>)
+
+    @Transaction
+    suspend fun updateCity(cities: List<CityEntity>) {
+        deleteCities()
+        insertCities(cities)
+    }
+
+    @Query("DELETE FROM CITYENTITY")
+    suspend fun deleteCities()
 
     @Query("SELECT * FROM CityEntity")
-    fun getAllCities(): List<CityEntity>
+    fun getAllCities(): Flow<List<CityEntity>>
 }
