e0c6f36e8a3fd77fbef060af3a4c182dce438eb6
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-04 23:06:07 +0330

    update UI Models

--- app/src/main/java/ir/divar/androidtask/feature/generic/uiState/PostItemUI.kt
+++ app/src/main/java/ir/divar/androidtask/feature/generic/uiState/PostItemUI.kt
@@ -3,8 +3,11 @@ package ir.divar.androidtask.feature.generic.uiState
 import androidx.compose.runtime.Immutable
 
 @Immutable
-data class PostItem(
-    val widgetType: String?, val data: PostDataItem?, val onItemClicked: (() -> Unit)?
+data class PostItemUI(
+    val id: Int? = 0,
+    val widgetType: String?,
+    val data: PostDataItemUI?,
+    val onItemClicked: (() -> Unit)?
 ) {
     enum class WidgetType(value: String) {
         TITLE_ROW("TITLE_ROW"),
