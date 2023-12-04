bb6bcba4567f7be0f79360afef79af2e12899127
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-04 16:38:32 +0330

    lock local datasource when query in database

--- app/src/main/java/ir/divar/androidtask/data/datasource/DefaultPlaceLocalDataSource.kt
+++ app/src/main/java/ir/divar/androidtask/data/datasource/DefaultPlaceLocalDataSource.kt
@@ -4,6 +4,8 @@ import ir.divar.androidtask.data.local.dao.CityDao
 import ir.divar.androidtask.data.local.entity.CityEntity
 import ir.divar.androidtask.data.repository.DispatcherProvider
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -12,15 +14,21 @@ class DefaultPlaceLocalDataSource @Inject constructor(
     private val dao: CityDao
 ) : PlaceLocalDataSource {
 
+    private val mutex = Mutex()
+
     override suspend fun getCityList(): Flow<List<CityEntity>> {
-        return withContext(dispatcher.io()) {
-            dao.getAllCities()
+        return mutex.withLock {
+            withContext(dispatcher.io()) {
+                dao.getAllCities()
+            }
         }
     }
 
     override suspend fun updateCities(cities: List<CityEntity>) {
-        return withContext(dispatcher.io()) {
-            dao.updateCity(cities)
+        return mutex.withLock {
+            withContext(dispatcher.io()) {
+                dao.updateCity(cities)
+            }
         }
     }
 }
--- app/src/main/java/ir/divar/androidtask/data/datasource/DefaultPostLocalDataSource.kt
+++ app/src/main/java/ir/divar/androidtask/data/datasource/DefaultPostLocalDataSource.kt
@@ -4,6 +4,8 @@ import ir.divar.androidtask.data.local.dao.PostDao
 import ir.divar.androidtask.data.local.entity.PostEntity
 import ir.divar.androidtask.data.repository.DispatcherProvider
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -12,19 +14,25 @@ class DefaultPostLocalDataSource @Inject constructor(
     private val dao: PostDao,
 ) : PostLocalDataSource {
 
+    private val mutex = Mutex()
+
     override suspend fun filterPosts(
         cityId: Int,
         page: Int,
         lastPostDate: Int
     ): Flow<List<PostEntity>> {
-        return withContext(dispatcher.io()) {
-            dao.filterPosts(cityId, page, lastPostDate)
+        return mutex.withLock {
+            withContext(dispatcher.io()) {
+                dao.filterPosts(cityId, page, lastPostDate)
+            }
         }
     }
 
     override suspend fun updatePosts(post: List<PostEntity>) {
-        withContext(dispatcher.io()) {
-            dao.updatePosts(post)
+        mutex.withLock {
+            withContext(dispatcher.io()) {
+                dao.updatePosts(post)
+            }
         }
     }
 }
