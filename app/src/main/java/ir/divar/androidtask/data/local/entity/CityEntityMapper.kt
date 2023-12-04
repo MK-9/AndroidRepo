b368dec1ed04d9fefc68f4a83a6419747f1908c9
Mohammad Soltanmohammadi <mrking9821@gmail.com>
2023-12-04 23:33:40 +0330

    create PostEntity and CityEntity Mapper

--- app/src/main/java/ir/divar/androidtask/data/local/entity/CityEntityMapper.kt
+++ app/src/main/java/ir/divar/androidtask/data/local/entity/CityEntityMapper.kt
@@ -1,9 +1,13 @@
 package ir.divar.androidtask.data.local.entity
 
+import ir.divar.androidtask.data.model.Centroid
+import ir.divar.androidtask.data.model.City
 import ir.divar.androidtask.data.network.models.CentroidDto
 import ir.divar.androidtask.data.network.models.CityDto
 
 object CityEntityMapper {
+
+    ////////////// ENTITY ---> DTO //////////////
     fun CityEntity.toCityDto() = CityDto(
         name = name, id = id, slug = slug, radius = radius, centroid = centroid?.toCentroidDto()
     )
@@ -12,11 +16,25 @@ object CityEntityMapper {
         latitude = latitude, longitude = longitude
     )
 
+    ////////////// DTO ---> ENTITY //////////////
     fun CityDto.toCityEntity() = CityEntity(
         name = name, id = id, slug = slug, radius = radius, centroid = centroid?.toCentroidEntity()
     )
 
     private fun CentroidDto.toCentroidEntity() = CentroidEntity(
+        latitude = latitude, longitude = longitude
+    )
+
+    ////////////// ENTITY ---> EXTERNAL MODEL //////////////
+    fun CityEntity.toCityExternalModel() = City(
+        name = name,
+        id = id,
+        slug = slug,
+        radius = radius,
+        centroid = centroid?.toCentroidExternalModel()
+    )
+
+    private fun CentroidEntity.toCentroidExternalModel() = Centroid(
         latitude = latitude, longitude = longitude
     )
 }
--- app/src/main/java/ir/divar/androidtask/data/local/entity/PostEntityMapper.kt
+++ app/src/main/java/ir/divar/androidtask/data/local/entity/PostEntityMapper.kt
@@ -2,6 +2,8 @@ package ir.divar.androidtask.data.local.entity
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import ir.divar.androidtask.data.model.Post
+import ir.divar.androidtask.data.model.PostData
 import ir.divar.androidtask.data.network.models.ImageItemDto
 import ir.divar.androidtask.data.network.models.PostDataDto
 import ir.divar.androidtask.data.network.models.PostDto
@@ -27,7 +29,7 @@ object PostEntityMapper {
         items = Gson().fromJson(items, object : TypeToken<List<ImageItemDto>>() {}.type)
     )
 
-    ///////////////////////////////////
+    ////////////// DTO ---> ENTITY //////////////
     fun PostDto.toPostEntity(
         cityId: Int = 0,
         page: String = "",
@@ -53,5 +55,29 @@ object PostEntityMapper {
         showThumbnail = showThumbnail,
         thumbnail = thumbnail,
         items = Gson().toJson(items)
+    )
+
+    ////////////// ENTITY ---> EXTERNAL MODEL //////////////
+    fun PostEntity.toPostExternalModel() = Post(
+        cityId = cityId,
+        page = page,
+        lastPostDate = lastPostDate,
+        widgetType = widgetType,
+        data = data?.toPostDataExternalModel(),
+    )
+
+    private fun PostDataEntity.toPostDataExternalModel() = PostData(
+        title = title,
+        subtitle = subtitle,
+        text = text,
+        value = value,
+        token = token,
+        price = price,
+        city = city,
+        district = district,
+        imageUrl = imageUrl,
+        showThumbnail = showThumbnail,
+        thumbnail = thumbnail,
+        items = null
     )
 }
